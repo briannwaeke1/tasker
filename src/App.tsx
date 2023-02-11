ab9a242fd1a6f30f495b8031a514027ce4170c5c
Brian Nwaeke <94656081+briannwaeke1@users.noreply.github.com>
2023-02-10 18:31:29 -0800

    create container, summary, and summaryItem components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import reactLogo from './assets/react.svg'
-
+import Container from "./components/Container";
+import Summary from "./components/Summary/Summary";
 
 function App() {
-
-
   return (
-   <div>Hello</div>
-  )
+    <div className="flex justify-center m-5">
+      <div className="flex flex-col items-center">
+        <div className="sm:w-[640px] border shadow p-10 flex flex-col gap-10">
+          <Container title={"Summary"}>
+            <Summary />
+          </Container>
+          <Container>
+            <div> Placeholder section</div>
+          </Container>
+          <Container title={"Placeholder"}>
+            <div> Placeholder section</div>
+          </Container>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
